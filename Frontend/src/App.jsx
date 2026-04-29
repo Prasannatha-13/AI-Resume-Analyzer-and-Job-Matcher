481e8006e4d03d315e764bd070f0d9fce3243fe4
Prasannatha-13 <banothuprasanna9@gmail.com>
2026-04-29 11:13:06 +0530

    App fuile added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import './App.css'
 import Navbar from "./components/Navbar.jsx"
+import Home from './components/Home.jsx'
+import Features from "./components/Features.jsx"
+import Howitworks from './components/Howitworks.jsx'
+import Results from './components/Results.jsx'
+import Aboutus from './components/aboutus.jsx'
 
 function App() {
   return(
   <div>
     <Navbar/>
+    <Home/>
+    <Howitworks/>
+    <Features/>
+    <Results/>
+    <Aboutus/>
   </div>
   )
 }
